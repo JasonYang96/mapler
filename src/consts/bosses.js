06d03e0a007c00c2d9f4362f37448118ebfd0cf7
Jason Yang <JasonYang96@gmail.com>
2020-10-12 16:10:53 -0700

    add other daily bosses

--- src/consts/bosses.js
+++ src/consts/bosses.js
@@ -1,14 +1,54 @@
-import zakumImg from '../images/zakum.png';
+import arkariumImg from '../images/arkarium.png';
+import crimsonQueenImg from '../images/crimsonQueen.png';
+import golluxImg from '../images/gollux.png';
 import hillaImg from '../images/hilla.png';
+import magnusImg from '../images/magnus.png';
+import pinkBeanImg from '../images/pinkBean.png';
+import ranmaruImg from '../images/ranmaru.png';
+import ursusImg from '../images/ursus.png';
+import vellumImg from '../images/vellum.png';
+import zakumImg from '../images/zakum.png';
 
 const bosses = [
   {
-    name: 'Zakum',
-    img: zakumImg
+    name: 'Arkarium',
+    img: arkariumImg
+  },
+  {
+    name: 'Crimson Queen',
+    img: crimsonQueenImg
+  },
+  {
+    name: 'Gollux',
+    img: golluxImg
   },
   {
     name: 'Hilla',
     img: hillaImg
+  },
+  {
+    name: 'Magnus',
+    img: magnusImg
+  },
+  {
+    name: 'Pink Bean',
+    img: pinkBeanImg
+  },
+  {
+    name: 'Ranmaru',
+    img: ranmaruImg
+  },
+  {
+    name: 'Ursus',
+    img: ursusImg
+  },
+  {
+    name: 'Vellum',
+    img: vellumImg
+  },
+  {
+    name: 'Zakum',
+    img: zakumImg
   }
 ]
 
